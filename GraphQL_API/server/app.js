79e9477d3f2f6fe17711c89fe2ce538ee3d5badc
breezybrow <browder.bree@gmail.com>
2022-01-27 11:23:40 -0600

    create a new GraphQLObjectType and array

--- GraphQL_API/server/app.js
+++ GraphQL_API/server/app.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const {graphqlHTTP} = require('express-graphql');
+// require the schema.js 
+const schema = require('./schema/schema.js')
 
 const app = express();
 
 app.use('/graphql',graphqlHTTP({
+  // add schema in an object
+  // add the property graphiql: true to constructor to be able to use GraphiQL
+  schema,
+  graphiql: true
 }));
 app.listen(4000,()=>{
   console.log('now listening for request on port 4000');
---
+++ GraphQL_API/server/schema/schema.js
@@ -0,0 +1,43 @@
+const express = require('express')
+const expressGraphQL = require('express-graphql')
+const lodash = require('lodash');
+
+const {
+  GraphQLSchema,
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLInt
+} = require('graphql')
+
+const app = express()
+
+// create an array tasks that contains these 2 different task objects
+const arrayTasks = [
+	{ id: 1, title: 'Create your first webpage', weight: 1, description: 'Create your first HTML file 0-index.html with: -Add the doctype on the first line (without any comment) -After the doctype, open and close a html tag Open your file in your browser (the page should be blank)'},
+	{ id: 2, name: 'Structure your webpage', weight: 1, description: 'Copy the content of 0-index.html into 1-index.html Create the head and body sections inside the html tag, create the head and body tags (empty) in this order'},
+]
+
+
+const TaskType = new GraphQLObjectType({
+  name: 'Task',
+  description: 'This represents a book written by an author',
+  fields: () => ({
+    id: { type: GraphQLNonNull(GraphQLString) },
+    title: { type: GraphQLNonNull(GraphQLString) },
+    weight: { type: GraphQLNonNull(GraphQLInt) },
+    description: { type: GraphQLNonNull(GraphQLString) },
+    fieldTask: {
+      type: TaskType,
+      resolve: (parent, args) => {
+        // leave empty
+      }
+    }
+  })
+})
+
+// export your GraphQLSchema with your RootQuery
+const schema = new GraphQLSchema({
+    query: RootQueryType,
+    mutation: RootMutationType
+})
+  
