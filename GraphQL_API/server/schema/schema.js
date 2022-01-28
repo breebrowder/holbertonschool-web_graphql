625c6b05496be3b3c83c4b4b70b300530ccb16cd
breezybrow <browder.bree@gmail.com>
2022-01-27 19:13:01 -0600

    push changes to test

--- GraphQL_API/server/schema/schema.js
+++ GraphQL_API/server/schema/schema.js
@@ -5,6 +5,7 @@ const lodash = require('lodash');
 const {
   GraphQLSchema,
   GraphQLObjectType,
+  GraphQLID,
   GraphQLString,
   GraphQLInt
 } = require('graphql')
@@ -13,10 +14,15 @@ const app = express()
 
 // create an array tasks that contains these 2 different task objects
 const arrayTasks = [
-	{ id: 1, title: 'Create your first webpage', weight: 1, description: 'Create your first HTML file 0-index.html with: -Add the doctype on the first line (without any comment) -After the doctype, open and close a html tag Open your file in your browser (the page should be blank)'},
-	{ id: 2, name: 'Structure your webpage', weight: 1, description: 'Copy the content of 0-index.html into 1-index.html Create the head and body sections inside the html tag, create the head and body tags (empty) in this order'},
+	{ id: '1', title: 'Create your first webpage', weight: 1, description: 'Create your first HTML file 0-index.html with: -Add the doctype on the first line (without any comment) -After the doctype, open and close a html tag Open your file in your browser (the page should be blank)'},
+	{ id: '2', name: 'Structure your webpage', weight: 1, description: 'Copy the content of 0-index.html into 1-index.html Create the head and body sections inside the html tag, create the head and body tags (empty) in this order'},
 ]
 
+// create an array project that contains these 2 different project objects
+const arrayProjects = [
+  { id: '1', title: 'Advanced HTML', weight: 1, description: 'Welcome to the Web Stack specialization. The 3 first projects will give you all basics of the Web development: HTML, CSS and Developer tools. In this project, you will learn how to use HTML tags to structure a web page. No CSS, no styling - don’t worry, the final page will be “ugly” it’s normal, it’s not the purpose of this project. Important note: details are important! lowercase vs uppercase / wrong letter… be careful!'},
+  { id: '2', title: 'Bootstrap', weight: 1, description: 'Bootstrap is a free and open-source CSS framework directed at responsive, mobile-first front-end web development. It contains CSS and JavaScript design templates for typography, forms, buttons, navigation, and other interface components.'},
+]
 
 const TaskType = new GraphQLObjectType({
   name: 'Task',
@@ -26,10 +32,27 @@ const TaskType = new GraphQLObjectType({
     title: { type: (GraphQLString) },
     weight: { type: (GraphQLInt) },
     description: { type: (GraphQLString) },
-    fieldTask: {
+    Task: {
       type: TaskType,
       resolve: (parent, args) => {
-        return lodash.find(fieldTask, { id: parent.fieldTaskId});
+        return lodash.find(arrayTasks, { id: parent.TaskId});
+      }
+    }
+  })
+})
+
+const ProjectType = new GraphQLObjectType({
+  name: 'Project',
+  description: 'This represents a book written by an author',
+  fields: () => ({
+    id: { type: (GraphQLID) },
+    title: { type: (GraphQLString) },
+    weight: { type: (GraphQLInt) },
+    description: { type: (GraphQLString) },
+    Project: {
+      type: ProjectType,
+      resolve: (parent, args) => {
+        return lodash.find(arrayProjects, { id: parent.ProjectId});
       }
     }
   })
@@ -38,15 +61,24 @@ const TaskType = new GraphQLObjectType({
 const RootQueryType = new GraphQLObjectType({
   name: 'RootQueryType',
   fields: () => ({
-    fieldTask: {
+    Task: {
     type: TaskType,
     args: {
         id: { type: GraphQLString }
       },
       resolve: (parent, args) => {
-      lodash.find(fieldTask, { id: args.id })
+      lodash.find(arrayTasks, { id: args.id })
       }
     },
+    Project: {
+      type: ProjectType,
+      args: {
+          id: { type: GraphQLString }
+        },
+        resolve: (parent, args) => {
+        lodash.find(arrayProjects, { id: args.id })
+        }
+      },
   })
 })
 
