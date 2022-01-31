b02427a7db06d868580760d7438ffb131a6add97
breezybrow <browder.bree@gmail.com>
2022-01-31 17:50:18 -0600

    For task 10

--- GraphQL_API/client/src/components/AddTask.js
+++ GraphQL_API/client/src/components/AddTask.js
@@ -1,96 +1,26 @@
-import {
-  useState,
-  //useEffect
-} from "react";
+import { getProjectsQuery } from '../queries/queries';
+import { graphql } from 'react-apollo';
 
 
-function AddTask(props) {
-  const [inputs, setInputs] = useState({
-    title: '',
-    weight: 1,
-    description: '',
-    projectId: ''
-  });
 
+  function displayProjects() {
+    //  console.log(props);
+    var data = props.data;
+    if (data.loading) {
+      return ( <option> Loading projects... </option>);
+      }
+      else {
+        return data.projects.map(project => {
+            return ( <option key = {
+                project.id
+              }
+              value = {
+                project.id
+              } > {
+                project.title
+              } </option>);
+            })
+        }
+      }
 
-  const handleChange = (e) => {
-    const newInputs = {
-      ...inputs
-    };
-    newInputs[e.target.name] = e.target.value
-    setInputs(newInputs)
-  }
-
-  return ( <
-    form class = "task"
-    id = "add-task"
-    /*onSubmit = {...}*/ >
-    <
-    div className = "field" >
-    <
-    label > Task title: < /label> <
-    input type = "text"
-    name = "title"
-    onChange = {
-      handleChange
-    }
-    value = {
-      inputs.title
-    }
-    required /
-    >
-    < /
-    div > <
-    div className = "field" >
-    <
-    label > Weight: < /label> <
-    input type = "number"
-    name = "weight"
-    onChange = {
-      handleChange
-    }
-    value = {
-      inputs.weight
-    }
-    required /
-    >
-    < /
-    div >
-    <
-    div className = "field" >
-    <
-    label > description: < /label> <
-    textarea name = "description"
-    onChange = {
-      handleChange
-    }
-    value = {
-      inputs.description
-    }
-    required /
-    >
-    < /
-    div >
-    <
-    div className = "field" >
-    <
-    label > Project: < /label> <
-    select name = "projectId"
-    onChange = {
-      handleChange
-    }
-    value = {
-      inputs.projectId
-    }
-    required > < option value = ""
-    selected = "selected"
-    disabled = "disabled" > Select project < /option>  < /
-    select > < /
-    div >
-    <
-    button > + < /button> < /
-    form >
-  );
-}
-
-export default AddTask;
+export default graphql(getProjectsQuery)(AddTask);
--- GraphQL_API/client/src/components/TaskList.js
+++ GraphQL_API/client/src/components/TaskList.js
@@ -3,7 +3,30 @@ import { graphql } from 'react-apollo';
 import TaskDetails from './TaskDetails';
 import { getTasksQuery } from '../queries/queries';
 
+function displayTasks() {
+  console.log(props.data);
+  var data = props.data;
 
+  if (data.loading) {
+    return ( <div> Loading tasks... </div>);
+    }
+    else {
+      return data.tasks.map(task => {
+          return ( <li key = {
+              task.id
+            }
+            onClick = {
+              (e) => {
+                setState({
+                  selected: task.id
+                });
+              }
+            } > {
+              task.title
+            } </li>);
+          })
+      }
+   }
 
 
 
